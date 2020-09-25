4f01dfa89cab596e094f82209885e625bf9f170c
Joshua Tan <josh.tan.tann@gmail.com>
2020-09-25 22:34:51 +0000

    Add test suite

--- breedFetcher.js
+++ breedFetcher.js
@@ -3,7 +3,7 @@ const request = require('request');
 const fetchBreedDescription = function(breedName, callback) {
   request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
     const data = JSON.parse(body);
-    
+
     if (!data[0]) {
       error = "Breed not found.";
       callback(error);
